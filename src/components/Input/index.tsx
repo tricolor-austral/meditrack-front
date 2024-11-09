336472acc29429b7b88fe4d0b82af2899019977b
pedropanosyan <pedro.panosyan@ing.austral.edu.ar>
2024-11-09 09:59:37 -0300

    selector and input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,6 @@
 import { cva, VariantProps } from 'class-variance-authority';
+import React from "react";
+import DeleteIcon from "../../assets/delete.svg";
 
 interface InputProps extends VariantProps<typeof inputStyles> {
   placeholder?: string;
@@ -7,20 +9,23 @@ interface InputProps extends VariantProps<typeof inputStyles> {
   size?: 'sm' | 'md' | 'lg';
   required?: boolean;
   variant?: 'primary' | 'secondary' | 'error';
+  showIcon?: boolean;
 }
 
-const inputStyles = cva('rounded transition-all block w-full h-auto px-4 border border-gray-400', {
+const inputStyles = cva(
+    'rounded transition-all block w-full h-auto px-4 border border-gray-400 focus:border-transparent',
+    {
   variants: {
     size: {
       sm: 'text-sm py-1 max-h-8',
       md: 'text-md py-2 max-h-10',
       lg: 'text-lg py-4 max-h-14',
     },
-      variant: {
-          primary: 'focus:border-primary',
-          secondary: 'focus:border-secondary',
-          error: 'focus:border-red',
-      },
+    variant: {
+      primary: 'focus:outline-primary',
+      secondary: 'focus:outline-secondary',
+      error: 'focus:outline-red focus:border-red-500',
+    },
     disabled: {
       true: 'opacity-50 cursor-not-allowed',
       false: '',
@@ -29,6 +34,10 @@ const inputStyles = cva('rounded transition-all block w-full h-auto px-4 border 
   defaultVariants: {
     size: 'md',
     variant: 'secondary',
+    disabled: false,
+    required: false,
+    type: 'text',
+    showIcon: true,
   } as never,
 });
 
@@ -39,12 +48,20 @@ export const Input: React.FC<InputProps> = ({
   placeholder,
   disabled,
   required,
+    showIcon,
 }) => (
-  <input
-    type={type}
-    className={inputStyles({ size, variant, disabled })}
-    placeholder={placeholder}
-    disabled={disabled}
-    required={required}
-  />
+    <div className="relative w-full">
+      <input
+          type={type}
+          className={`${inputStyles({size, disabled, variant})}`}
+          placeholder={placeholder}
+          disabled={disabled}
+          required={required}
+      />
+      {showIcon && (
+          <span className="absolute inset-y-0 right-3 flex items-center cursor-pointer">
+          <img src={DeleteIcon} alt="Clear" className="h-4 w-4"/>
+        </span>
+      )}
+    </div>
 );
---
+++ src/components/Selector/Selector.stories.tsx
@@ -0,0 +1,59 @@
+import type { StoryObj } from '@storybook/react';
+import { Selector } from './index.tsx';
+import '../../index.css';
+
+const meta = {
+    title: 'Components/Selector',
+    component: Selector,
+    tags: ['autodocs'],
+    parameters: {
+        controls: {
+            expanded: true,
+        },
+    },
+    args: {
+        variant: 'primary',
+        size: 'md',
+        disabled: false,
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: { type: 'select' },
+        },
+        variant: {
+            options: ['primary', 'secondary', 'error'],
+            control: { type: 'select' },
+        },
+        disabled: {
+            control: { type: 'boolean' },
+        },
+        onChange: {
+            action: 'onChange',
+        },
+        options: {
+            control: { type: 'object' },
+        },
+        selected: {
+            control: { type: 'text' },
+        },
+        isExpanded: {
+            control: {type: 'boolean'},
+        },
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof Selector>;
+
+// Story definitions
+export const Default: Story = {
+    args: {
+        options: [
+            { label: 'Option 1', value: '1' },
+            { label: 'Option 2', value: '2' },
+            { label: 'Option 3', value: '3' },
+        ],
+        selected: '1',
+    },
+};
