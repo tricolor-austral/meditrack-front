d9bbda381a64a2efa07a492e94060cf9fb74bb7f
zartf <tomas.alcazar@ing.austral.edu.ar>
2024-11-12 21:01:26 -0300

    add the favorite doctors

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -2,19 +2,24 @@ import React, { useState } from 'react';
 import { Icon } from '../components/Icons';
 import {Input} from "../components/Input";
 import {MedicContact} from "../components/MedicContact";
+import d1 from '../assets/doctors/d1.svg';
+import d4 from '../assets/doctors/d4.svg';
+import d5 from '../assets/doctors/d5.svg';
+import d6 from '../assets/doctors/d6.svg';
+import d7 from '../assets/doctors/d7.svg';
+import d8 from '../assets/doctors/d8.svg';
 
 const medics = [
-    { name: 'Liza Trays', email: 'lizatrass@mail.com', specialty: 'Cardiologist' },
-    { name: 'Ale Spielb', email: 'ale@mail.com', specialty: 'Neurologist' },
-    { name: 'Liam Johnson', email: 'l.johnson@mail.com', specialty: 'Orthopedic' },
-    { name: 'Michael Brown', email: 'm..brown@mail.com', specialty: 'Dermatologist' },
-    { name: 'Liza Trays', email: 'lizatrass@mail.com', specialty: 'Cardiologist' },
-    { name: 'Ale Spielb', email: 'ale@mail.com', specialty: 'Neurologist' },
-    { name: 'Liam Johnson', email: 'l.johnson@mail.com', specialty: 'Orthopedic' },
-    { name: 'Michael Brown', email: 'm..brown@mail.com', specialty: 'Dermatologist' },
+  { name: 'Liza Trays', email: 'lizatrass@mail.com', specialty: 'Cardiologist', imgSrc: d1 },
+  { name: 'Liam Johnson', email: 'l.johnson@mail.com', specialty: 'Orthopedic', imgSrc: d4 },
+  { name: 'Michael Brown', email: 'm..brown@mail.com', specialty: 'Dermatologist', imgSrc: d5 },
+  { name: 'Emily Stone', email: 'emily.stone@mail.com', specialty: 'Ophthalmologist', imgSrc: d6 },
+  { name: 'George Smith', email: 'george.smith@mail.com', specialty: 'ENT Specialist', imgSrc: d7 },
+  { name: 'Olivia Lee', email: 'olivia.lee@mail.com', specialty: 'Gastroenterologist', imgSrc: d8 },
 ];
+
 const FavoritesPage: React.FC = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm] = useState('');
 
   const filteredMedics = medics.filter(medic =>
     medic.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -35,8 +40,8 @@ const FavoritesPage: React.FC = () => {
             }}/>
         </div>
         <div className={'max-h-[600px] overflow-scroll flex-col gap-4 flex mb-[80px]'}>
-            {filteredMedics.map((medic, index) => (
-                <MedicContact name={medic.name} email={medic.email} imgSrc={''}/>
+            {filteredMedics.map((medic) => (
+                <MedicContact name={medic.name} email={medic.email} imgSrc={medic.imgSrc}/>
             ))}
         </div>
     </div>
